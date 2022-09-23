ae83010ad9363a090be5ea62fa62817416b1d9e5
Mika Järvenpää <mika.jarvenpaa@vincit.fi>
2022-09-23 15:44:19 +0300

    add signin

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,22 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
+import SignOut from './SignOut';
 
-const Nav = () => (
-  <NavStyles>
-    <Link href="/">Home</Link>
-    <Link href="/products">Products</Link>
-    <Link href="/orders">Orders</Link>
-    <Link href="/sell">Sell</Link>
-    <Link href="/account">Account</Link>
-  </NavStyles>
-);
-
-export default Nav;
+export default function Nav() {
+  const user = useUser();
+  return (
+    <NavStyles>
+      <Link href="/">Home</Link>
+      <Link href="/products">Products</Link>
+      {user && (
+        <>
+          <Link href="/orders">Orders</Link>
+          <Link href="/sell">Sell</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user ? <Link href="/signin">Sign in</Link> : <SignOut />}
+    </NavStyles>
+  );
+}
